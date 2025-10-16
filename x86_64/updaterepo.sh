fafe8cbfe6b30435530da8f1d97c82efce19de0b
LorgOS <lorgeolas@proton.me>
2025-10-16 22:14:14 +0100

    Updating repo
    Fixing typos / aligning naming

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rm lorgeolas_repo*
+rm lorgos_repo*
 
 echo "repo-add"
-repo-add -n -R lorgeolas_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R lorgos_repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm lorgeolas_repo.db
+rm lorgos_repo.db
 
-rm lorgeolas_repo.files
+rm lorgos_repo.files
 
-mv lorgeolas_repo.db.tar.gz lorgeolas_repo.db
+mv lorgos_repo.db.tar.gz lorgos_repo.db
 
-mv lorgeolas_repo.files.tar.gz lorgeolas_repo.files
+mv lorgos_repo.files.tar.gz lorgos_repo.files
 
 echo "####################################"
 echo "Repo Updated!!"
